cd3dd2e17bf207f796f2e6c88f4c57591f1fb03d
Kohei-kun-no <k.minoda1205@iCloud.com>
2022-03-28 21:31:59 +0900

    うp

--- lib/main.dart
+++ lib/main.dart
@@ -483,7 +483,7 @@ class _PrefetchImageDemoState extends State<PrefetchImageDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('To Grasses')),
+      appBar: AppBar(title: Text('To')),
       body: Container(
           child: CarouselSlider.builder(
             itemCount: images.length,
